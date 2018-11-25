07b5c519c3d5e5d47d24a6311068ea839fab9901
George Wilde <george@wildecreations.co.uk>
2018-11-25 22:56:09 +0000

    Improve display of the menu on mobile devices.

--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -1,31 +1,90 @@
 export class Navbar {
-    private $window = $(window);
-    private $navbar = $('.navbar');
-    private initialNavbarHeight = this.$navbar.outerHeight();
-
-    public initialise(): void {
-        this.navbarResize();
+    private static getViewportWidth(): number {
+        return Math.max(
+            document.documentElement.clientWidth,
+            document.documentElement.offsetWidth,
+            document.documentElement.scrollWidth,
+        );
     }
 
-    private navbarResize(): void {
-        this.$window.on('scroll', () => {
-            const scrollTopPosition = this.$window.scrollTop();
-            const skillsElementVerticalPosition = $('#skills').offset().top - this.initialNavbarHeight;
-            const scrollPositionOffset = -1;
-            const navbarSizeOffset = 10;
+    private readonly widthToShowDesktopMenu = 768;
+    private readonly scrollPositionOffset = -1;
+    private readonly navbarSizeOffset = 10;
 
-            if (scrollTopPosition + scrollPositionOffset <= skillsElementVerticalPosition) {
-                this.makeNavbarBigAndTransparent();
-            }
+    private $window = $(window);
+    private $navbar = $('.navbar');
+    private $navbarToggler = $('.navbar-toggler');
+    private $navbarCollapse = $('.navbar-collapse');
+    private $navLink = $('.nav-link');
 
-            if (scrollTopPosition + scrollPositionOffset > skillsElementVerticalPosition) {
-                this.makeNavbarOpaque();
-            }
-
-            if (scrollTopPosition + scrollPositionOffset > skillsElementVerticalPosition + navbarSizeOffset) {
-                this.makeNavbarSmall();
-            }
+    public initialise() {
+        this.$window.on('scroll load resize', () => {
+            this.resizeNavbar();
         });
+
+        this.$navbarToggler.on('click', () => {
+            this.toggleMenu();
+        });
+
+        this.$navLink.on('click', () => {
+            this.collapseMenu();
+        });
+    }
+
+    private resizeNavbar() {
+        const scrollTopPosition = this.$window.scrollTop();
+        const initialNavbarHeight = this.$navbar.outerHeight() - this.$navbarCollapse.outerHeight();
+        const skillsElementVerticalPosition = $('#skills').offset().top - initialNavbarHeight;
+        const menuIsCollapsed = this.$navbarToggler.hasClass('collapsed');
+
+        const viewportWidth = Navbar.getViewportWidth();
+
+        if (scrollTopPosition + this.scrollPositionOffset <= skillsElementVerticalPosition && menuIsCollapsed) {
+            this.makeNavbarBigAndTransparent();
+        }
+
+        if (scrollTopPosition + this.scrollPositionOffset > skillsElementVerticalPosition) {
+            this.makeNavbarOpaque();
+        }
+
+        if (
+            (
+                scrollTopPosition + this.scrollPositionOffset >
+                skillsElementVerticalPosition + this.navbarSizeOffset
+            ) &&
+            (viewportWidth >= this.widthToShowDesktopMenu)
+        ) {
+            this.makeNavbarSmall();
+        }
+    }
+
+    private toggleMenu() {
+        const viewportWidth = Navbar.getViewportWidth();
+        const goingToShowMenu = this.$navbarToggler.hasClass('collapsed');
+        const scrollTopPosition = this.$window.scrollTop();
+        const initialNavbarHeight = this.$navbar.outerHeight() - this.$navbarCollapse.outerHeight();
+        const skillsElementVerticalPosition = $('#skills').offset().top - initialNavbarHeight;
+
+        if (viewportWidth < this.widthToShowDesktopMenu) {
+            if (goingToShowMenu) {
+                this.makeNavbarOpaque();
+            } else {
+                if (scrollTopPosition + this.scrollPositionOffset <= skillsElementVerticalPosition) {
+                    console.log('makeNavbarBigAndTransparent');
+                    this.makeNavbarBigAndTransparent();
+                }
+            }
+        }
+    }
+
+    private collapseMenu() {
+        const menuIsCollapsed = this.$navbarToggler.hasClass('collapsed');
+        const $navbarCollapse = $('.navbar-collapse') as any;
+
+        if (!menuIsCollapsed) {
+            this.toggleMenu();
+            $navbarCollapse.collapse('hide');
+        }
     }
 
     private makeNavbarOpaque() {
